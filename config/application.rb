7df7ee5d26c56bbd0b0e7e0c638c44f11255dd4c
Marcos Tsuzuki <mtsuzuki@usp.br>
2020-10-13 20:21:41 -0300

    Devise foi estilizado com Bootstrap.

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Meca0d
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.active_record.schema_format = :sql
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
