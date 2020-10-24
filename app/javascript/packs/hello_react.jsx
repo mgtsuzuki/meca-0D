da6cc9bf71908042361d066f1692d90158640c91
Andre Sato <andre.kubagawa@usp.br>
2020-10-24 10:58:44 -0300

    Remove react hooks, change to class

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import axios from 'axios';
 
@@ -6,29 +6,32 @@ const PATH = '/users/api';
 const PARAM_KEYWORD = 'keywords=';
 const PARAM_PAGE = 'page=';
 
-function App() {
-    const [hits, setHits] = useState([]);
-    const [page, setPage] = useState(0);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [isBottom, setIsBottom] = useState(false);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hits: [],
+            page: 0,
+            searchTerm: '',
+            isBottom: false
+        };
+        this.fetchData = this.fetchData.bind(this);
+        this.handleScroll = this.handleScroll.bind(this);
+      }
 
-    async function fetchData(searchTerm, page = 0, reset = false) {
+    async fetchData(searchTerm, page = 0, reset = false) {
         const result = await axios(`${PATH}?${PARAM_KEYWORD}${searchTerm}&${PARAM_PAGE}${page}`,);
-        setHits(reset? result.data : [...hits,...result.data]);
-        setPage(page);
-        setIsBottom(false);
+        this.setState(prevState => { 
+            const updatedHits = reset? result.data : [...prevState.hits,...result.data]
+            return {
+                hits: updatedHits,
+                page: page,
+                isBottom: false
+            };
+        });
     }
 
-    useEffect(() => {    
-        fetchData(searchTerm, page);
-    }, []);
-    
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
-    
-    function handleScroll() {
+    handleScroll() {
         const scrollTop = (document.documentElement
           && document.documentElement.scrollTop)
           || document.body.scrollTop;
@@ -36,59 +39,73 @@ function App() {
           && document.documentElement.scrollHeight)
           || document.body.scrollHeight;
         if (scrollTop + window.innerHeight + 50 >= scrollHeight){
-          setIsBottom(true);
+          this.setState({isBottom: true});
         }
     }
 
-    useEffect(() => {
-        if (isBottom) {
-            fetchData(searchTerm, page + 1);
-        }
-    }, [isBottom]);
+    componentDidMount() {    
+        this.fetchData(this.state.searchTerm, this.state.page);
+        window.addEventListener('scroll', this.handleScroll);
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
 
-    return (
-        <div>
-            <header>
-                <h1 className="h2">User Search</h1>
-            </header>
-            <section className="search-form">
-                <form onSubmit={(event) => {
-                    fetchData(searchTerm, 0, true);
-                    event.preventDefault();
-                }}>
-                    <div className="input-group input-group-lg">
-                        <label className="sr-only">Keywords</label>
-                        <input type="text" placeholder="First Name, Last Name, or Email Address" className="form-control input-lg"
-                            value={searchTerm}
-                            onChange={(event) => setSearchTerm(event.target.value)}
-                        />
-                        <span className="input-group-btn">
-                            <button type="submit" className="btn btn-primary btn-lg">
-                                Find Customers
-                            </button>
-                        </span>
-                    </div>
-                </form>
-            </section>
-            <section className="search-results">
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.isBottom !== this.state.isBottom) {
+            if (this.state.isBottom) {
+                this.fetchData(this.state.searchTerm, this.state.page + 1);
+            }
+        }
+    }
+
+    render() {
+        const {hits, searchTerm} = this.state;
+        return (
+            <div>
                 <header>
-                    <h1 className="h3">Results</h1>
+                    <h1 className="h2">User Search</h1>
                 </header>
-                <ol className="list-group">
-                    {hits.map(user => (
-                        <li className="list-group-item" key={user.id}>
-                            <h3 className="float-right">
-                                <small className="text-uppercase">Joined </small>
-                                {user.created_at.slice(0,10)}
-                            </h3>
-                            <h2 className="h3">{user.first_name} {user.last_name}</h2>
-                            <h4>{user.email}</h4>
-                        </li>
-                    ))}
-                </ol>
-            </section>
-        </div>
-    );
+                <section className="search-form">
+                    <form onSubmit={(event) => {
+                        this.fetchData(searchTerm, 0, true);
+                        event.preventDefault();
+                    }}>
+                        <div className="input-group input-group-lg">
+                            <label className="sr-only">Keywords</label>
+                            <input type="text" placeholder="First Name, Last Name, or Email Address" className="form-control input-lg"
+                                value={searchTerm}
+                                onChange={ event => {this.setState({searchTerm: event.target.value})} }
+                            />
+                            <span className="input-group-btn">
+                                <button type="submit" className="btn btn-primary btn-lg">
+                                    Find Customers
+                                </button>
+                            </span>
+                        </div>
+                    </form>
+                </section>
+                <section className="search-results">
+                    <header>
+                        <h1 className="h3">Results</h1>
+                    </header>
+                    <ol className="list-group">
+                        {hits.map(user => (
+                            <li className="list-group-item" key={user.id}>
+                                <h3 className="float-right">
+                                    <small className="text-uppercase">Joined </small>
+                                    {user.created_at.slice(0,10)}
+                                </h3>
+                                <h2 className="h3">{user.first_name} {user.last_name}</h2>
+                                <h4>{user.email}</h4>
+                            </li>
+                        ))}
+                    </ol>
+                </section>
+            </div>
+        );
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
